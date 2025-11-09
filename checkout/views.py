8ab9a9be90c88fb3c6137748688ea94d94b7b868
14sammie41 <q85e7u2t@students.codeinstitute.net>
2025-11-09 20:10:13 +0000

    Found issues in basket to checkout definitions to be fixed

--- checkout/views.py
+++ checkout/views.py
@@ -1,60 +1,77 @@
-import stripe
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
+
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+from marketplace.models import Product
+
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from marketplace.models import Product
-from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from forms import OrderForm
-from .models import Order, OrderLineItem
+
+import stripe
 
 def checkout(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    
+    basket = request.session.get('basket', {})
+    
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
+            line_items = []
+            order_total = 0
+            
             order = form.save(commit=False)
-            order.original_basket = request.session.get('basket', {})
-            order.stripe_pid = request.POST.get('stripe_pid')
+            order.original_basket = basket
             order.save()
             
-            product = Product.objects.first()  # Example: get a product
-            line_item = OrderLineItem(
-                order=order,
-                product=product,
-                quantity=1
-            )
-            line_item.save()
-            
-            session = stripe.checkout.Session.create(
-                payment_method_types=['card'],
-                line_items=[{
+            for item_id, item_data in basket.items():
+                product = get_object_or_404(Product, pk=item_id)
+                line_total = product.price * quantity
+                order_total += line_total
+                
+                OrderLineItem.objects.create(
+                    order=order,
+                    product=product,
+                    quantity=quantity
+                )
+                
+                line_items.append({
                     'price_data': {
                         'currency': 'gbp',
-                        'unit_amount': int(product.price * 100),
                         'product_data': {
                             'name': product.name,
                         },
+                        'unit_amount': int(product.price * 100),
                     },
-                    'quantity': 1,
-                }],
+                    'quantity': quantity,
+                })
+            order.total = order_total
+            order.save()
+
+            session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=line_items,
                 mode='payment',
                 success_url=request.build_absolute_uri(
                     reverse('checkout_success', args=[order.order_number])
                 ),
-                cancel_url=request.build_absolute_uri(reverse('checkout_cancel')),
+                cancel_url=request.build_absolute_uri(
+                    reverse('checkout_home')
+                ),
             )
-            
             order.stripe_pid = session.payment_intent
             order.save()
             
             return redirect(session.url, code=303)
-    else:
-        form = OrderForm()
         
-    return render(request, 'checkout/checkout.html', {'form': form})
+        return render(request, 'checkout/checkout.html', {'form': form, 'basket': basket})
+    
+    form = OrderForm()
+    return render(request, 'checkout/checkout.html', {'form': form, 'basket': basket})
 
 def checkout_success(request, order_number):
+    order_number = request.session.get('order_number')
     order = get_object_or_404(Order, order_number=order_number)
     return render(request, 'checkout/checkout_success.html', {'order': order})
-
-stripe.api_key = settings.STRIPE_SECRET_KEY
