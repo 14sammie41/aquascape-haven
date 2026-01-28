fb602e0cb132af0fe606eae606387acf526a9680
Samantha Spencer <14sammie41@gmail.com>
2026-01-28 21:28:56 +0000

    Added the correct URL for Webhooks in checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('cancel/', views.cancel, name='cancel'),
     path('order/<order_number>/', views.order_detail, name='order_detail'),
+    path('wh/', views.stripe_webhook, name='stripe_webhook'),
     path('success/<order_number>/', views.success, name='success'),
 ]
